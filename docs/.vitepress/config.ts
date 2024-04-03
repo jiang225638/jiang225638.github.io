6a3bddff0f30371534a3b84934eaecc2aab432cd
zhujiang <jiang225638@163.com>
2024-04-03 17:13:30 +0800

    修改docs中的config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,7 +8,7 @@ const APP_BASE_PATH = basename(process.env.GITHUB_REPOSITORY || '')
 
 export default defineConfig({
   outDir: '../dist',
-  base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
+  base: APP_BASE_PATH ? `/${APP_BASE_PATH}/blob/gh-pages/` : '/',
 
   lang: 'zh-CN',
   title: '茂茂物语',
