863d134f1ae558dcdd99d54fbccdb28156c74b31
zhujiang <jiang225638@163.com>
2024-04-03 11:49:27 +0800

    提交语

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,60 @@
+import { basename } from 'node:path'
+import { defineConfig } from 'vitepress'
+import MarkdownPreview from 'vite-plugin-markdown-preview'
+
+import { head, nav, sidebar } from './configs'
+
+const APP_BASE_PATH = basename(process.env.GITHUB_REPOSITORY || '')
+
+export default defineConfig({
+  outDir: '../dist',
+  base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
+
+  lang: 'zh-CN',
+  title: '茂茂物语',
+  description: '茂茂的成长之路，包含前端常用知识、源码阅读笔记、各种奇淫技巧、日常提效工具等',
+  head,
+
+  lastUpdated: true,
+  cleanUrls: true,
+
+  /* markdown 配置 */
+  markdown: {
+    lineNumbers: true,
+  },
+
+  /* 主题配置 */
+  themeConfig: {
+    i18nRouting: false,
+
+    logo: '/logo.png',
+
+    nav,
+    sidebar,
+    /* 右侧大纲配置 */
+    outline: {
+      level: 'deep',
+      label: '本页目录',
+    },
+
+    socialLinks: [{ icon: 'github', link: 'https://github.com/maomao1996/vitepress-nav-template' }],
+
+    footer: {
+      message: '如有转载或 CV 的请标注本站原文地址',
+      copyright: 'Copyright © 2019-present maomao',
+    },
+
+    darkModeSwitchLabel: '外观',
+    returnToTopLabel: '返回顶部',
+    lastUpdatedText: '上次更新',
+
+    docFooter: {
+      prev: '上一篇',
+      next: '下一篇',
+    },
+  },
+
+  vite: {
+    plugins: [MarkdownPreview()],
+  },
+})
