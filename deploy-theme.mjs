31e561a4ecf2193f03af6fedb5fb4a738ad0722b
zhujiang <jiang225638@163.com>
2025-02-24 17:10:18 +0800

    12321321

---
+++ deploy-theme.mjs
@@ -0,0 +1,24 @@
+#!/usr/bin/env zx
+
+// user config
+const originName = 'theme'
+
+// not care
+const compressPkgName = `${originName}.tar.gz`
+const user = 'root'
+const origin = 'sugarat.top'
+const fullOrigin = `${originName}.${origin}`
+const baseServerDir = '/www/wwwroot'
+const destDir = ''
+
+await $`pnpm buildTheme`
+
+await $`echo ==🔧 压缩dist ==`
+await $`cd packages/theme/docs/.vitepress && tar -zvcf ${compressPkgName} dist && rm -rf dist && mv ${compressPkgName} ./../../../../`
+
+await $`echo ==🚀 上传到服务器 ==`
+await $`scp ${compressPkgName} ${user}@${fullOrigin}:./`
+await $`rm -rf ${compressPkgName}`
+
+await $`echo ==✅ 部署代码 ==`
+await $`ssh -p22 ${user}@${fullOrigin} "tar -xf ${compressPkgName} -C ${baseServerDir}/${fullOrigin}/${destDir}"`
---
+++ deploy.mjs
@@ -0,0 +1,25 @@
+#!/usr/bin/env zx
+
+// user config
+const originName = 'next'
+
+// not care
+const compressPkgName = `${originName}.tar.gz`
+const user = 'root'
+const origin = 'sugarat.top'
+const fullOrigin = `${originName}.${origin}`
+const baseServerDir = '/www/wwwroot'
+const destDir = ''
+
+await $`pnpm build`
+
+await $`echo ==🔧 压缩dist ==`
+await $`cd packages/blogpress/.vitepress && tar -zvcf ${compressPkgName} dist && rm -rf dist && mv ${compressPkgName} ./../../../`
+
+await $`echo ==🚀 上传到服务器 ==`
+await $`scp ${compressPkgName} ${user}@${fullOrigin}:./`
+await $`rm -rf ${compressPkgName}`
+
+await $`echo ==✅ 部署代码 ==`
+await $`ssh -p22 ${user}@${fullOrigin} "tar -xf ${compressPkgName} -C ${baseServerDir}/${fullOrigin}/${destDir}"`
+// await $`ssh -p22 ${user}@${origin} "tar -xf ${compressPkgName} -C ${baseServerDir}/blog.${origin}/${destDir}"`
